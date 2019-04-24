fbd35c2e408cabe47396b9964f2d9b042443a129
Xiaoyu Huang <007pig@gmail.com>
2019-04-24 22:17:08 +0800

    Appserver is working

--- appserver.go
+++ appserver.go
@@ -1,1 +1,47 @@
 package appserver
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AppServer struct {
+	router  *gin.Engine
+	httpsrv *http.Server
+}
+
+func NewAppServer() *AppServer {
+	srv := &AppServer{router: gin.Default()}
+	srv.buildRoutes()
+	return srv
+}
+
+func (srv *AppServer) buildRoutes() {
+
+}
+
+func (srv *AppServer) Start() {
+
+	srv.httpsrv = &http.Server{
+		Addr:    ":8080",
+		Handler: srv.router,
+	}
+
+	go func() {
+		if err := srv.httpsrv.ListenAndServe(); err != http.ErrServerClosed {
+			// Error starting or closing listener:
+			log.Printf("HTTP server ListenAndServe: %v", err)
+		}
+	}()
+}
+
+func (srv *AppServer) Stop() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return srv.httpsrv.Shutdown(ctx)
+}
